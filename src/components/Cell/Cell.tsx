d321f434a31c0d32fcb78591697fe9ed9d737bc5
Pablo Sebastián Caballero <pablocaballero1295@gmail.com>
2025-01-09 15:55:21 +0000

    Created status on each cell

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -3,17 +3,32 @@ import styles from "./Cell.module.css"
 
 interface CellProps {
   letter: string
+  status: string
 }
 
-export const Cell = ({ letter }: CellProps) => {
+export const Cell = ({ letter, status }: CellProps) => {
   const [value, setValue] = useState(letter)
 
   useEffect(() => {
     setValue(letter)
   }, [letter])
 
+  const getStyle = () => {
+    let cell_style = styles.cell
+
+    if (status == "incorrect") {
+      cell_style += ` ${styles.incorrect}`
+    } else if (status == "correct") {
+      cell_style += ` ${styles.correct}`
+    } else if (status == "included") {
+      cell_style += ` ${styles.included}`
+    }
+
+    return cell_style
+  }
+
   return (
-    <div className={styles.cell}>
+    <div className={getStyle()}>
       <div className={styles.cell_value}>{value}</div>
     </div>
   )
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -6,22 +6,31 @@ import { useState } from "react"
 
 //const base_grid = ["     ", "     ", "     ", "     ", "     ", "     "]
 
+type CellState = {
+  value: string
+  status: string
+}
+
 const createGrid = (n_rows: number, word_size: number) => {
-  const grid: string[][] = []
+  const grid: CellState[][] = []
   for (let i = 0; i < n_rows; i++) {
     grid[i] = []
     for (let j = 0; j < word_size; j++) {
-      grid[i].push("")
+      grid[i].push({ value: "", status: "" })
     }
   }
   return grid
 }
 
+const solution = "ROYAL"
+
 export const MainPage = () => {
   const n_rows = 6
   const word_size = 5
 
-  const [tries, setTries] = useState(createGrid(n_rows, word_size))
+  const [tries, setTries] = useState<CellState[][]>(
+    createGrid(n_rows, word_size)
+  )
   const [activeRow, setActiveRow] = useState(0)
   const [activeColumn, setActiveColumn] = useState(0)
 
@@ -32,7 +41,7 @@ export const MainPage = () => {
     setTries((prevMatrix) => {
       const updatedMatrix = [...prevMatrix]
       const updatedRow = [...updatedMatrix[activeRow]]
-      updatedRow[activeColumn] = value
+      updatedRow[activeColumn].value = value
       updatedMatrix[activeRow] = updatedRow
       return updatedMatrix
     })
@@ -45,7 +54,7 @@ export const MainPage = () => {
     setTries((prevMatrix) => {
       const updatedMatrix = [...prevMatrix]
       const updatedRow = [...updatedMatrix[activeRow]]
-      updatedRow[activeColumn - 1] = ""
+      updatedRow[activeColumn - 1].value = ""
       updatedMatrix[activeRow] = updatedRow
       return updatedMatrix
     })
@@ -58,18 +67,45 @@ export const MainPage = () => {
 
   const handleKeyboardButton = (value: string) => {
     if (value == "delete" || value == "del") {
-      removeLetter()
+      if (activeColumn != 0) {
+        removeLetter()
+      }
     } else if (value == "guess" || value == "check") {
       console.log("CHECKING")
       let word = ""
-      tries[activeRow].map((val) => {
-        word = word + val
+
+      const _tries = [...tries]
+
+      _tries[activeRow].map((val) => {
+        word = word + val.value
       })
       if (word.length >= word_size) {
+        for (let i = 0; i < word_size; i++) {
+          console.log(
+            _tries[activeRow][i].value.toUpperCase(),
+            solution[i].toUpperCase()
+          )
+
+          if (
+            _tries[activeRow][i].value.toUpperCase() ==
+            solution[i].toUpperCase()
+          ) {
+            _tries[activeRow][i].status = "correct"
+          } else if (
+            solution
+              .toUpperCase()
+              .includes(_tries[activeRow][i].value.toUpperCase())
+          ) {
+            _tries[activeRow][i].status = "included"
+          } else {
+            _tries[activeRow][i].status = "incorrect"
+          }
+        }
+
+        setTries(_tries)
+
         setActiveRow(activeRow + 1)
         setActiveColumn(0)
-      } else {
-        console.log("SHORT WORD")
       }
     } else {
       addLetter(value)
@@ -83,7 +119,7 @@ export const MainPage = () => {
         {tries.map((item, i) => (
           <div key={i} className={styles.words_row}>
             {item.map((letter, j) => (
-              <Cell key={j} letter={letter} />
+              <Cell key={j} letter={letter.value} status={letter.status} />
             ))}
           </div>
         ))}
